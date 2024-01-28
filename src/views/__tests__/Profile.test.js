894fda6f040f380ebe613f54df64880074dcd585
mike-subtilis <mike@subtilis-software.com>
2024-01-28 10:36:42 -0700

    fixing tests

--- src/views/__tests__/Profile.test.js
+++ src/views/__tests__/Profile.test.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { ProfileComponent } from "../Profile";
-import { useAuth0 } from "@auth0/auth0-react";
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ProfilePage } from '../ProfilePage.jsx';
+import { useAuth0 } from '@auth0/auth0-react';
 
-jest.mock("@auth0/auth0-react");
+jest.mock('@auth0/auth0-react');
 
-describe("The profile component", () => {
+describe('The profile component', () => {
   beforeEach(() => {
     useAuth0.mockReturnValue({
       loading: false,
       user: {
-        name: "Test user",
-        email: "test@user.com",
-        picture: "https://avatar.com",
+        name: 'Test user',
+        email: 'test@user.com',
+        picture: 'https://avatar.com',
       },
     });
   });
 
-  it("renders when loading = true", () => {
-    render(<ProfileComponent />);
+  it('renders when loading = true', () => {
+    render(<ProfilePage />);
   });
 });

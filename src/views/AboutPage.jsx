0fab0f4a5d50df117f17ef7d62c44f278dc9ea0d
mike-subtilis <mike@subtilis-software.com>
2024-04-05 15:27:36 -0600

    about page cleanup

--- src/views/AboutPage.jsx
+++ src/views/AboutPage.jsx
@@ -22,8 +22,8 @@ const HomePage = () => {
           </Heading>
         </CardHeader>
         <CardBody>
-          AnswerBrawl will (at some point) present you with 2 possible answers to a question.
-          Simply select the one that answers the question best for you between the two.
+          AnswerBrawl presents you with 2 possible answers to a question.
+          Simply select one of the answers.
         </CardBody>
       </Card>
 
@@ -35,7 +35,8 @@ const HomePage = () => {
           </Heading>
         </CardHeader>
         <CardBody>
-          AnswerBrawl will (at some point) compile all votes and present a ranking of all of the answers.
+          AnswerBrawl compiles all votes and presents a ranking of all
+          potential answers to a question.
         </CardBody>
       </Card>
 
@@ -50,7 +51,6 @@ const HomePage = () => {
           You can ask a new question, supply it with a ton of potential answers, and then begin voting
           on those questions. You can choose to make the question public or private. You&apos;ll
           need to be signed up and logged in to ask a new question.
-          (Currently you can ask a new question without answers if you sign up)
         </CardBody>
       </Card>
 
